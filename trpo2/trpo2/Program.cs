6dbf5a1729e5de804864beff30753bad789460c8
Malka Schildt <malka_88@outlook.com>
2019-03-12 17:20:16 +0700

    base

---
+++ trpo2/trpo2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trpo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b;
+            int x;
+            Console.WriteLine("Enter A and B: ");
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Choose the operation:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+            x = int.Parse(Console.ReadLine());
+
+            switch (x)
+            {
+                case 1:
+                    add(a, b);
+                    break;
+                case 2:
+                    sub(a, b);
+                    break;
+                case 3:
+                    mul(a, b);
+                    break;
+                case 4:
+                    div(a, b);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
